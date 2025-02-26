9e62ff8adcc05eefc276774e248a69463f0d75af
shogo21 <shogo21arai@gmail.com>
2025-02-26 13:49:08 +0900

    Update datagenerator.py

--- datagenerator.py
+++ datagenerator.py
@@ -12,11 +12,12 @@ class CustomDataGenerator(Sequence):
         self.image_size = image_size
         self.indexes = np.arange(len(self.data_list))
         self.max_sequence_num = max_sequence_num
+        self.on_epoch_end()  #first shuffle
 
-    #エポックごとに何回__getitem__を呼び出すのか
     def __len__(self):
         return int(np.ceil(len(self.data_list) / self.batch_size))
 
+    #バッチサイズごとにデータをロード
     def __getitem__(self, index):
         batch_indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
         batch_data = [self.data_list[i] for i in batch_indexes]
@@ -25,40 +26,71 @@ class CustomDataGenerator(Sequence):
         images = []
         labels = []
         bright = random.uniform(0.3, 2.0)
-        #パスから画像を開く
         for sequence in batch_data:
           sequence_images = [np.array(Image.open(img_path).resize(self.image_size)) for img_path in sequence]
 
           #label = list(map(lambda p: (1 if "touch" in p else 0), sequence))
 
-          #系列データの長さを揃える
           if len(sequence_images) < self.max_sequence_num:
             zero_imagearray = [np.zeros((64, 64, 3)) for i in range(self.max_sequence_num-len(sequence_images))]
             sequence_images = sequence_images + zero_imagearray
 
           sequence_images = np.array(sequence_images)
-          sequence_images = sequence_images / 255.0
-          images.append(sequence_images)
-
-          #色相変換によるデータ拡張
-          change_images1 = cv2.convertScaleAbs(sequence_images*255, alpha=bright, beta=0)
-          change_images1 = np.array(change_images1)
-          change_images1 = change_images1 / 255.0
-          #元のデータ+データ拡張
+          change_images1 = sequence_images / 255.0
+          change_images1 = self.normal(change_images1)
           images.append(change_images1)
 
-          """
-          images.append(sequence_images)
-          images.append(change_images1)"""
-
+          #データ拡張
+          change_images2 = cv2.convertScaleAbs(sequence_images, alpha=bright, beta=0)
+          change_images2 = np.array(change_images2)
+          change_images2 = change_images2 / 255.0
+          change_images2 = self.normal(change_images2)
+          images.append(change_images2)
+            
         for label in batch_labels:
           if len(label) < self.max_sequence_num:
             label = label + [0]*(self.max_sequence_num-len(label))
           label = np.array(label)
           label = label.reshape((-1, 1))
           labels.append(label)
-          #データ拡張した場合ラベルも追加する
           for i in range(1):
               labels.append(label)
 
         return np.array(images), np.array(labels)
+
+    #学習データシャッフル
+    def on_epoch_end(self):
+        #shuffle train data every one epoch
+        np.random.shuffle(self.indexes)
+
+    #標準化
+    def normal(self, sequence):
+        updated_sequence = []
+        for i, data in enumerate(sequence):
+            data = data.astype('float32')
+
+            R,G,B = np.dsplit(data, 3)
+            R = np.squeeze(R)
+            if np.std(R) != 0:
+                R = (R - np.mean(R)) / np.std(R)*0.166+0.5
+            else:
+                R = np.ones_like(R) * 0.5
+
+            G = np.squeeze(G)
+            if np.std(G) != 0:
+                G = (G - np.mean(G)) / np.std(G)*0.166+0.5
+            else:
+                G = np.ones_like(G) * 0.5
+
+            B = np.squeeze(B)
+            if np.std(B) != 0:
+                B = (B - np.mean(B)) / np.std(B)*0.166+0.5
+            else:
+                B = np.ones_like(B) * 0.5
+
+            data = np.stack([R, G, B], 2)
+            data = np.clip(data, 0.0, 1.0)
+            
+            updated_sequence.append(data)
+
+        return np.array(updated_sequence)
