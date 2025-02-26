209563d45903d34e130392b9d1bade9f3e6aa50d
shogo21 <shogo21arai@gmail.com>
2025-02-26 13:54:49 +0900

    Update val_datagenerator.py

--- val_datagenerator.py
+++ val_datagenerator.py
@@ -30,11 +30,10 @@ class ValDataGenerator(Sequence):
           if len(sequence_images) < self.max_sequence_num:
             zero_imagearray = [np.zeros((64, 64, 3)) for i in range(self.max_sequence_num-len(sequence_images))]
             sequence_images = sequence_images + zero_imagearray
-
-          #bright = random.uniform(0.3, 2.0)
+              
           sequence_images = np.array(sequence_images)
-          #sequence_images = cv2.convertScaleAbs(sequence_images, alpha=bright, beta=0)
           sequence_images = sequence_images / 255.0
+          sequence_images = self.normal(sequence_images)
           images.append(sequence_images)
 
         for label in batch_labels:
@@ -47,3 +46,35 @@ class ValDataGenerator(Sequence):
               labels.append(label)"""
 
         return np.array(images), np.array(labels)
+
+    #標準化
+    def normal(self, sequence):
+        updated_sequence = []
+        for i, data in enumerate(sequence):
+            data = data.astype('float32')
+
+            R,G,B = np.dsplit(data, 3)
+            R = np.squeeze(R)
+            if np.std(R) != 0:
+                R = (R - np.mean(R)) / np.std(R)*0.166+0.5
+            else:
+                R = np.ones_like(R) * 0.5
+
+            G = np.squeeze(G)
+            if np.std(G) != 0:
+                G = (G - np.mean(G)) / np.std(G)*0.166+0.5
+            else:
+                G = np.ones_like(G) * 0.5
+
+            B = np.squeeze(B)
+            if np.std(B) != 0:
+                B = (B - np.mean(B)) / np.std(B)*0.166+0.5
+            else:
+                B = np.ones_like(B) * 0.5
+
+            data = np.stack([R, G, B], 2)
+            data = np.clip(data, 0.0, 1.0)
+            
+            updated_sequence.append(data)
+
+        return np.array(updated_sequence)
